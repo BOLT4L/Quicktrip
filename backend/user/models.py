a2449dcd57fc0a8c36f13f195a97b16450a59ec5
BOLT4L <Eyobwasihun5@gmail.com>
2025-03-30 16:57:17 +0300

    update 1.1

--- backend/user/models.py
+++ backend/user/models.py
@@ -38,6 +38,7 @@ class user(AbstractBaseUser,PermissionsMixin):
     user_type = models.CharField(max_length=12 , choices=type.choices, default=type.USER)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     phone_number = models.BigIntegerField(null=False,default=0,unique=True)
